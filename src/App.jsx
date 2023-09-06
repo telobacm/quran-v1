d59000b0581be19f707dc9537321fa47573c923a
telobacm <telobacm@gmail.com>
2023-09-06 13:00:32 +0700

    second

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,19 @@ const baseURL = "https://quran-api-gadingnst-two.vercel.app/";
 function App() {
   const [surahData, setSurahData] = useState(null);
   const [surahNumber, setSurahNumber] = useState(1);
-  const [verse, setVerse] = useState(null);
+  const [verse, setVerse] = useState(1);
 
   useEffect(() => {
-    axios.get(baseURL+'surah/'+surahNumber).then((response) => {
+    surahNumber !== '' && axios.get(baseURL+'surah/'+surahNumber).then((response) => {
       setSurahData(response.data.data);
     });
   }, [surahNumber]);
 
   if (!surahData) return null;
 
-  async function handleVerse(e) {
-    // let ayat = surahData.verses.filter((item) => item.number.inSurah === 5)
-    await setVerse(surahData.verses.filter((item) => item.number.inSurah === e.target.value))
-    console.log(verse);
+  function handleVerse(e) {
+    console.log(surahData.verses.some(item => item.number.inSurah === parseInt(e.target.value)))
+    surahData.verses.some(item => item.number.inSurah === parseInt(e.target.value)) ? setVerse(surahData.verses.filter((item) => item.number.inSurah === parseInt(e.target.value))) : console.log('verse number invalid')
   }
 
   return (
@@ -38,8 +37,10 @@ function App() {
             Enter verse number: <input type="number" min="1" defaultValue={1} name="surahNumber" onChange={handleVerse} />
         </label>
       </form>
-      {surahData.verses.map((item) => <li>{item.text.arab}</li>)}
-      {/* {verse !== null && <p>{verse[0].text.arab}</p>} */}
+      {verse !== null && <p>{verse[0].text.arab}</p>}
+      {/* <div>
+        {surahData.verses.map((item, index) => <li key={index}>{item.text.arab}</li>)}
+      </div> */}
     </div>
     </>
   )

d153163a0c2d0c71b474cb0e3ea29ce0850ab2ff
telobacm <telobacm@gmail.com>
2023-09-08 13:51:44 +0700

    adding fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect} from 'react'
 import axios from 'axios'
-import './App.css'
 
 const baseURL = "https://quran-api-gadingnst-two.vercel.app/";
 
 function App() {
   const [surahData, setSurahData] = useState(null);
   const [surahNumber, setSurahNumber] = useState(1);
-  const [verse, setVerse] = useState(1);
+  const [verse, setVerse] = useState(null);
 
   useEffect(() => {
     surahNumber !== '' && axios.get(baseURL+'surah/'+surahNumber).then((response) => {
@@ -23,26 +22,29 @@ function App() {
   }
 
   return (
-    <>
-      <form>
+    <div className='mx-auto px-12'>
+      <form className='text-center'>
         <label>
           Enter surah number: <input value={surahNumber} type="number" min="1" max="114"  name="surahNumber" onChange={e => setSurahNumber(e.target.value)} />
         </label>
       </form>
-    <div>
-      <h1>{surahData.name.long}</h1>
-      <p>revelation: {surahData.revelation.arab} - {surahData.revelation.en} - {surahData.revelation.id}</p>
-      <form>
-        <label>
-            Enter verse number: <input type="number" min="1" defaultValue={1} name="surahNumber" onChange={handleVerse} />
-        </label>
-      </form>
-      {verse !== null && <p>{verse[0].text.arab}</p>}
-      {/* <div>
-        {surahData.verses.map((item, index) => <li key={index}>{item.text.arab}</li>)}
-      </div> */}
+      <div className='text-center'>
+        <h1 className='font-uthmani text-4xl'>{surahData.name.long}</h1>
+        <p className='text-4xl'>{surahData.name.long}</p>
+        <p className='text-xl font-poppins'>revelation: {surahData.revelation.arab} - {surahData.revelation.en} - {surahData.revelation.id}</p>
+        <form>
+          <label className='text-lg'>
+              Enter verse number: <input type="number" min="1" defaultValue={1} name="surahNumber" onChange={handleVerse} />
+          </label>
+        </form>
+        {verse !== null && <p>{verse[0].text.arab}</p>}
+        <div>
+          <div className='grid gap-6'>
+            {surahData.verses.map((item, index) => <p className='text-2xl text-right' key={index}>{item.text.arab}</p>)}
+          </div>
+        </div>
+      </div>
     </div>
-    </>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  extend: {
+    colors: {
+      primary: {
+      }
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      adelia: ["ADELIA", "cursive"],
+      uthmani: ["UthmanTaha", "cursive"],
+      tangerine: ["Tangerine", "serif"]
+    },
+  },
+  plugins: [],
+}
+
